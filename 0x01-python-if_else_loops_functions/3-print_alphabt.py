2274791ea86828c32bcbcc8a3b1e524126f1a66a
Elizabeth <testlolabtest@gmail.com>
2022-11-29 05:41:54 -0800

    Include last letter of alphabet in range

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(97, 122):
+for i in range(97, 123):
     if (i == 101):
         continue
-    elif (i == 113):
+    if (i == 113):
         continue
     print("{:c}".format(i), end="")
