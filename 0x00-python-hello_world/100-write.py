678a16cd48cda3dcb76e5f7315027f3885671f4f
Elizabeth <testlolabtest@gmail.com>
2022-11-30 00:25:45 -0800

    Edit script to meet pycodestyle standards

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 import sys
-import subprocess
-def print_to_stderror():
-    sys.stderr.write('and that piece of art is useful - Dora Korpar, 2015-10-19\n')
-    sys.exit(1)
-print_to_stderror()
+sys.stderr.write('and that piece of art is useful - Dora Korpar, 2015-10-19\n')
+sys.exit(1)
