1b1d1986127d28086d6cd486406eccd09427e104
Elizabeth <testlolabtest@gmail.com>
2022-11-30 05:05:31 -0800

    Fix modulo answers for negative values

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,14 @@
 import random
 number = random.randint(-10000, 10000)
 
-if ((number % 10) > 5):
-    print(f"Last digit of {number} is {number % 10} and is greater than 5")
-elif ((number % 10) == 0):
+if number > 0:
+    the_last = number % 10
+else:
+    the_last = (abs(number) % 10) * -1
+
+if (the_last > 5):
+    print(f"Last digit of {number} is {the_last} and is greater than 5")
+elif (the_last == 0):
     print(f"Last digit of {number} is {number % 10} and is 0")
-elif ((number + (number // 10) * 10) < 6):
-    print(f"Last digit of {number} is {number + (number // 10) * 10} and is less than 6 and not 0")
+elif (the_last < 6):
+    print(f"Last digit of {number} is {the_last} and is less than 6 and not 0")
