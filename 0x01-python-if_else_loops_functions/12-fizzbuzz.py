2fc4dfa1cd28606e4134b2b3cbc8c4e7b8af4bcf
Elizabeth <testlolabtest@gmail.com>
2022-11-30 06:48:47 -0800

    Increase range

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1, 100):
+    for i in range(1, 101):
         if (i % 3 == 0):
             print("{}".format('Fizz'), end=" ")
-        elif (i % 5 == 0):
+        if (i % 5 == 0):
             print("{}".format('Buzz'), end=" ")
         if (i % 3 == 0 and i % 5 == 0):
             print("{}".format('FizzBuzz'), end=" ")
