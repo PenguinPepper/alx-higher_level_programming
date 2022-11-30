3ba31756adf6b56a51bbb681cc472fefe5df3b06
Elizabeth <testlolabtest@gmail.com>
2022-11-30 06:45:57 -0800

    Add condition for multiples of 5

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,9 @@ def fizzbuzz():
     for i in range(1, 100):
         if (i % 3 == 0):
             print("{}".format('Fizz'), end=" ")
-        elif (i % 3 == 0 and i % 5 == 0):
+        elif (i % 5 == 0):
+            print("{}".format('Buzz'), end=" ")
+        if (i % 3 == 0 and i % 5 == 0):
             print("{}".format('FizzBuzz'), end=" ")
         else:
             print("{}".format(i), end=" ")
