40c9f4b4769a7a211003ae7078a26539c3e66bdb
Elizabeth <testlolabtest@gmail.com>
2022-11-30 04:52:57 -0800

    Print number instead of letters

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,8 +3,8 @@ import random
 number = random.randint(-10000, 10000)
 
 if ((number % 10) > 5):
-    print(f"Last digit of {number} is {number % 10} and is greater than five")
+    print(f"Last digit of {number} is {number % 10} and is greater than 5")
 elif ((number % 10) == 0):
-    print(f"Last digit of {number} is {number % 10} and is zero")
+    print(f"Last digit of {number} is {number % 10} and is 0")
 elif ((number + (number // 10) * 10) < 6):
-    print(f"Last digit of {number} is {number + (number // 10) * 10} and is less than six and not zero")
+    print(f"Last digit of {number} is {number + (number // 10) * 10} and is less than 6 and not 0")
