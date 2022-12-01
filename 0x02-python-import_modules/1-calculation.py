61b37338fa8da9acd1a598208925e715dfa306c2
Elizabeth <testlolabtest@gmail.com>
2022-12-01 02:32:38 -0800

    Make code executable only when program is executed directly by interpreter

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import calculator_1 as calculator
-a = 10
-b = 5
-adds = calculator.add
-subs = calculator.sub
-multi = calculator.mul
-dvd = calculator.div
-print("{:d} + {:d} = {}".format(a, b, adds(a, b)))
-print("{:d} - {:d} = {}".format(a, b, subs(a, b)))
-print("{:d} * {:d} = {}".format(a, b, multi(a, b)))
-print("{:d} / {:d} = {}".format(a, b, dvd(a, b)))
+if __name__ == "__main__":
+    import calculator_1 as calculator
+    a = 10
+    b = 5
+    adds = calculator.add
+    subs = calculator.sub
+    multi = calculator.mul
+    dvd = calculator.div
+    print("{:d} + {:d} = {}".format(a, b, adds(a, b)))
+    print("{:d} - {:d} = {}".format(a, b, subs(a, b)))
+    print("{:d} * {:d} = {}".format(a, b, multi(a, b)))
+    print("{:d} / {:d} = {}".format(a, b, dvd(a, b)))
