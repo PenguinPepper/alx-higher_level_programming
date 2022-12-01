6060c87cb34da97d6bd5d753d58184eb999541f6
Elizabeth <testlolabtest@gmail.com>
2022-12-01 02:23:57 -0800

    Change type in string format

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import calculator_1 as calculator
+a = 10
+b = 5
+adds = calculator.add
+subs = calculator.sub
+multi = calculator.mul
+dvd = calculator.div
+print("{:d} + {:d} = {}".format(a, b, adds(a, b)))
+print("{:d} - {:d} = {}".format(a, b, subs(a, b)))
+print("{:d} * {:d} = {}".format(a, b, multi(a, b)))
+print("{:d} / {:d} = {}".format(a, b, dvd(a, b)))
