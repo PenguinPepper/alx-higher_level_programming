947f70fc992af4a9b7e20d892404f67705f247ee
Elizabeth <testlolabtest@gmail.com>
2022-12-05 02:04:38 -0800

    Add condition to skip number 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     list_last = len(my_list) + 1
-    for i in range(1, list_last):
+    for i in range(list_last):
+        if i == 0:
+            continue
         print("{:d}".format(i))
