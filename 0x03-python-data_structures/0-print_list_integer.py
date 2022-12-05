d5f64853b3a898097c4fad641976d521c4cd3008
Elizabeth <testlolabtest@gmail.com>
2022-12-05 00:43:39 -0800

    Add 1 to print last digit in list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    list_length = len(my_list)
-    for i in range(0, list_length):
+    list_length = len(my_list) + 1
+    for i in range(list_length):
         print("{:d}".format(i))
