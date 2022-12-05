7e0785b7d7a81316f7ce3f44fa4812a9888de150
Elizabeth <testlolabtest@gmail.com>
2022-12-05 02:01:45 -0800

    Change range from 0 to 1

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    list_length = len(my_list) + 1
-    for i in range(list_length):
+    list_last = len(my_list) + 1
+    for i in range(1, list_last):
         print("{:d}".format(i))
