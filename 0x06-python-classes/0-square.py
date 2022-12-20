7444d7f3c677d7a34518e41834502df7cbf69b76
Elizabeth <testlolabtest@gmail.com>
2022-12-20 10:17:03 -0800

    Fix Docstrings

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-"""This module contains the class definition called Square
-
-Square: empty class Square that defines a square
-"""
 class Square:
     """
-    Empty class of Square
+    Empty class definition of Square
     """
     pass
