089475363edd66e1ff48d080932a12a1ba5f058c
Elizabeth <testlolabtest@gmail.com>
2022-12-20 10:12:18 -0800

    Add module that defines a class named square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module contains the class definition called Square
+
+Square: empty class Square that defines a square
+"""
+class Square:
+    """Empty class of Square"""
+    pass
