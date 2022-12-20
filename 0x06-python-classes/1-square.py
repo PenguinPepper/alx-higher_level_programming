81a52dbeaa9fcc60992dd9d3e9acfec8149bae1e
Elizabeth <testlolabtest@gmail.com>
2022-12-20 11:26:28 -0800

    Add modules with private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Mode that defines a class called Square"""
+
+
+class Square:
+    """
+    A class Square that defines a square by: (based on 0-square.py).
+    """
+    def __init__(self, size):
+        """
+        Instantiation of attributes called size
+
+        Args:
+            size: The size of a square.
+        """
+        self.__size = size
