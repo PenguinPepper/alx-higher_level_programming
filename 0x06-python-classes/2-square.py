0438d51e825622c9ef4a2578cb88c16f2db4899c
Elizabeth <testlolabtest@gmail.com>
2022-12-28 07:25:13 -0800

    Edit according to pycodestyle

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""Module contains class that defines a square"""
+"""Module contains class that defines a square
+"""
+
+
 class Square:
     ''' Class that defines a square
     '''
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         '''Initialize a variable called size
 
         Args:
