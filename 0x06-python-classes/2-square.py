515babe6f2921181ab4f9f53e4c227aeca6284a1
Elizabeth <testlolabtest@gmail.com>
2022-12-28 07:19:26 -0800

    Add Class called Square that makes sure size is an integer

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module contains class that defines a square"""
+class Square:
+    ''' Class that defines a square
+    '''
+    def __init__(self, size = 0):
+        '''Initialize a variable called size
+
+        Args:
+            size (int): Size of the square must be an integer
+
+        Attributes:
+            size (int): Size of the square
+        '''
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
