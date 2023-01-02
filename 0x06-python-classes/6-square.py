292dc717251d2e1c00dcfe0ffb308b6a89cc499f
Elizabeth <testlolabtest@gmail.com>
2023-01-02 05:50:41 -0800

    Add condition to position setter to or instead of and

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -39,9 +39,9 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if type(value) is not tuple or value[0] < 0:
+        if type(value) is not tuple or len(value) != 2:
             raise TypeError('position must be a tuple of 2 positive integers')
-        elif len(value) != 2 or value[1] < 0:
+        elif value[0] or value[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
         elif type(value[0]) is not int or type(value[1]) is not int:
             raise TypeError('position must be a tuple of 2 positive integers')
