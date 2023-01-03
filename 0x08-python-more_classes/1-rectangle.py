f447d0d84b2b07c65e35b66027259236a3aba83f
Elizabeth <testlolabtest@gmail.com>
2023-01-03 11:02:37 -0800

    Remove elifs

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -28,10 +28,9 @@ class Rectangle:
     def height(self, value):
         if type(value) is not int:
             raise TypeError('height must be an integer')
-        elif value < 0:
+        if value < 0:
             raise ValueError('height must be >= 0')
-        else:
-            self.__height = value
+        self.__height = value
 
     @property
     def width(self):
@@ -46,7 +45,6 @@ class Rectangle:
     def width(self, value):
         if type(value) is not int:
             raise TypeError('width must be an integer')
-        elif value < 0:
+        if value < 0:
             raise ValueError('width must be >= 0')
-        else:
-            self.__width = value
+        self.__width = value
