418277777a8f539a6d56cb930d303ba7caaac614
Elizabeth <testlolabtest@gmail.com>
2023-01-07 07:55:12 -0800

    Fix code to handle float overflow

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,10 +12,9 @@ def add_integer(a, b=98):
     '''
     if type(a) not in [int, float]:
         raise TypeError('a must be an integer')
-    elif type(b) not in [int, float]:
+    if type(b) not in [int, float]:
         raise TypeError('b must be an integer')
-    if type(a) == float:
+    else:
         a = int(a)
-    elif type(b) == float:
         b = int(b)
     return int(a + b)
