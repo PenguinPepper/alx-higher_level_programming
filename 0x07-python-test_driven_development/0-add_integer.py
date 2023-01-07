d9aafa536cc5a36d39ed5e40393f0763fe729e0b
Elizabeth <testlolabtest@gmail.com>
2023-01-07 07:50:32 -0800

    Add file with a function that adds 2 integers and its test file

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''This module contains a dunction called add_integer'''
+def add_integer(a, b=98):
+    '''Function that adds two integers a and b
+
+    Args:
+        a (int): can be an integer or a float
+        b (int): can be an integer or float 
+
+    Returns:
+        The addition of a and b as an int type
+    '''
+    if type(a) not in [int, float]:
+        raise TypeError('a must be an integer')
+    elif type(b) not in [int, float]:
+        raise TypeError('b must be an integer')
+    if type(a) == float:
+        a = int(a)
+    elif type(b) == float:
+        b = int(b)
+    return int(a + b)
