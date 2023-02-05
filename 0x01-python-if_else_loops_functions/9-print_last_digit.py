227cff69ba4ee2bcc5b180d61f1ef94aec5f8915
Elizabeth <testlolabtest@gmail.com>
2023-02-05 06:34:18 -0800

    Fix function to accommadate pythons way of handling negative modulo numbers

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if (number > 0):
-        the_last = number % 10
+    if number < 0:
+        new_num = number * -1
+        the_last = (-1) * (new_num % 10)
+        the_last = the_last * -1
     else:
-        the_last = abs(number) % 10
+        the_last = number % 10
+    print("{:d}".format(the_last), end='')
     return (the_last)
