82ea16c05132820c8ef9fa2af90af7b1283d83fb
Elizabeth <testlolabtest@gmail.com>
2023-02-16 04:00:40 -0800

    Edit to plural form of exist

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,6 @@
 -- a script that creates the database hbtn_0d_usa and the table cities
-CREATE DATABASE IF NOT EXIST hbtn_0d_usa;
-CREATE TABLE IF NOT EXIST hbtn_0d_usa.cities(
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(256),s
-	state_id INT NOT NULL, FOREIGN KEY (states) REFERENCES states(id));
+	name VARCHAR(256),
+	state_id INT NOT NULL, FOREIGN KEY (state_id) REFERENCES states(id));
