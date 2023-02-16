bb876caa793f4c916d444315bfc0b247438db848
Elizabeth <testlolabtest@gmail.com>
2023-02-16 01:41:38 -0800

    Include database name in table creation

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,3 @@
 -- a script that creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(id INT NOT NULL AUTO-INCREMENT UNIQUE, name VARCHAR(256) NOT NULL, PRIMARY KEY (id));
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
