b6d34c5e791b790365ffd653db42d3fd10dd1e2b
Elizabeth <testlolabtest@gmail.com>
2023-02-16 04:03:35 -0800

    Reference correct table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,4 +3,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(256),
-	state_id INT NOT NULL, FOREIGN KEY (state_id) REFERENCES states(id));
+	state_id INT NOT NULL, FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id));
