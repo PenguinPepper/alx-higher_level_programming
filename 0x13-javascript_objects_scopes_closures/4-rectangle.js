323462a26df9c69f0799667191ec4956cf099154
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-03-14 21:08:59 +0000

    Addd script with rectangle class and 3 methods

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const w = this.width;
+    const h = this.height;
+    for (let i = 0; i < h; i++) {
+      console.log('X'.repeat(w));
+    }
+  }
+
+  rotate () {
+    const placeHolder = this.height;
+    this.height = this.width;
+    this.width = placeHolder;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
