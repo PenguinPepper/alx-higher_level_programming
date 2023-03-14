9fa1559f8715291294063fcbe3adc9cb08b0bb59
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-03-14 04:00:15 +0000

    Add script to calculate factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+function getFactorial (num) {
+  num = parseInt(process.argv[2]);
+  if (isNaN(num)) {
+   console.log(1);
+  } else {
+   console.log(num * getFactorial(num - 1));
+  }
+}
+getFactorial();
