e1b0054bb2326ed7ee671548ba8923d729195c13
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-03-16 05:01:19 +0000

    Edit to meet semistandard standard

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const leng = list.length - 1;
-  const empty =[];
+  const empty = [];
   for (let i = leng; i >= 0; i--) {
     empty.push(list[i]);
   }
-return empty
+  return empty;
 };
