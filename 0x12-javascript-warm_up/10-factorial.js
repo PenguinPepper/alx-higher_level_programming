2017306a81c77c3abafb432f42f0b02429130479
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-03-16 05:55:41 +0000

    Add script that changes value of global scope variable

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 function getFactorial (num) {
   num = parseInt(process.argv[2]);
-  if (isNaN(num)) {
-    console.log(1);
-  } else if (num > 90) {
-    console.log(num * getFactorial(num - 1));
+  if (num === 0) {
+     return(1);
   }
+  return (num * getFactorial(num - 1));
 }
+
 getFactorial();
--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,11 +2,12 @@
 if (process.argv[3]) {
   const arry = [];
   const leng = process.argv.length - 1;
-  for (let i = leng; i > 0; i--) {
+  for (let i = leng; i > 1; i--) {
     arry.push(process.argv[i]);
   }
   arry.sort();
   console.log(arry[arry.length - 2]);
+  console.log(arry);
 } else {
   console.log(0);
 }
