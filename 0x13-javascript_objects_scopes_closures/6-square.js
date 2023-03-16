7f5d272a468b9ad376a74f8ed815b18c365a8820
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-03-16 04:57:46 +0000

    Add Square class that inherits from square and prints it

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
 const Square1 = require('./5-square');
 class Square extends Square1 {
-  charPrint(c) {
-  for (let i = 0; i < this.width; i++) {
-   if (c === 'C') {
-     console.log('C'.repeat(this.width));
-   }
-   console.log('X'.repeat(this.width));
+  charPrint (c) {
+    for (let i = 0; i < this.width; i++) {
+      if (c) {
+        console.log(c.repeat(this.width));
+      } else {
+        console.log('X'.repeat(this.width));
+      }
+    }
   }
- }
 }
+module.exports = Square;
--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  const leng = list.length - 1
+  const leng = list.length - 1;
   const empty =[];
   for (let i = leng; i >= 0; i--) {
     empty.push(list[i]);
