1c834596e264edebaa79885bd798bbc39d1d1da3
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-03-17 13:04:43 -0700

    Ad file for task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""
+write a script that takes in arguments and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument and is MYSQL injection safe
+"""
