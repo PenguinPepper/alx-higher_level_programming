17e4d881fa15d146953db127c7a52912082e1c6b
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-03-19 03:51:42 -0700

    Script that lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,3 +2,16 @@
 """
 script that lists all states from the database hbtn_0e_0_usa
 """
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    databse = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                              passwd=sys.argv[2], db=sys.argv[3])
+    cur = databse.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    cur.close()
+    databse.close()
