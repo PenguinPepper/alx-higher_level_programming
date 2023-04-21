9b2613ca8a92a01eeaf06d4ab961e2123479f977
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-04-21 09:42:46 -0700

    Add the specific unique header

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # bash script that takes in URL and returns the size of the body of the response
-curl -s -w "% {size_download}" "$1"
+curl -s -o /dev/null 2 -w '%{http_code}' $1 | grep -q '200' && curl -s -I -w '%{size_download}\n' $1
