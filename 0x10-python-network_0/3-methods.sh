95827cafe6bd7f3baa4d61f0df976364ee7ecc8c
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-04-22 02:12:49 -0700

    Use awk to print out the headers allowed

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that displays all HTTP methods the server of a url will accept
-curl -s $1 -D /dev/null | grep -q "allow"
+curl -s -I $1 | grep "Allow" | awk '{print $2, $3, $4}'
