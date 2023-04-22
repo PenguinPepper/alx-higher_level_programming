8305fb444dd2ad3b821aa9e76e4f8ddd4daff414
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-04-22 08:39:04 -0700

    Add script that returns the http status without piping the output

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that displays all HTTP methods the server of a url will accept
-curl -s -I $1 | grep "Allow" | awk '{print $2, $3, $4}'
+curl -s -I $1 | grep "Allow" | arg=('$@') awk '{print [$@]}'
---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Function that finds a peak in an unsorted list
+"""
+
+def find_peak(list_of_integers):
+    # find the length of the list
+    # FOR i in length - 1
+        # max = 0
+        # IF list_integers[i] > list_integers[i + 1]
+            #then max = list_integers[i]
+    # return max
+    maxi = 0
+    length = list_of_integers.length
+    for i in length - 1:
+
