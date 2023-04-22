a7a95ada548c885ab3804a76548243eadf3c250b
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-04-22 00:58:59 -0700

    Order the flags correctly to get the desired output

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # bash script that takes in URL and returns the size of the body of the response
-curl -s -o /dev/null 2 -w '%{http_code}' $1 | grep -q '200' && curl -s -I -w '%{size_download}\n' $1
+curl -s $1 -o /dev/null -w "%{size_download}\n"
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that displays all HTTP methods the server of a url will accept
-curl -s -D "$1" | grep -q "allow"
+curl -s $1 -D /dev/null | grep -q "allow"
