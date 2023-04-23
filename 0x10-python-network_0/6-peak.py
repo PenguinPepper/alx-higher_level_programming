7e4cdef228574b8da23fb51780913e69b5312026
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-04-23 09:53:15 -0700

    Add code tha has a better time complexity

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,7 +7,7 @@ Function that finds a peak in an unsorted list
 def find_peak(list_of_integers):
     """
     Function that finds the peak in an unsorted array
-    """
+
     maxi = 0
     for i in range(len(list_of_integers)):
         if list_of_integers[i] > maxi:
@@ -15,3 +15,13 @@ def find_peak(list_of_integers):
     if maxi == 0:
         return None
     return maxi
+    """
+    mid= len(list_of_integers) // 2
+    if not list_of_integers:
+        return None
+    if list_of_integers[mid] > list_of_integers[mid + 1] and list_of_integers[mid] > list_of_integers[mid - 1]:
+        return list_of_integers[mid]
+    elif list_of_integers[mid] < list_of_integers[mid + 1] and list_of_integers[mid] > list_of_integers[mid - 1]:
+        return list_of_integers[mid + 1]
+    else:
+        return max(list_of_integers)
