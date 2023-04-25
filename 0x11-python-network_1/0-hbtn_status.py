3ecf4164a3ad3868eccac7df800d587dd5e25e0d
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-04-25 03:47:58 -0700

    Add first draft of python script for task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,11 @@ Script that fetches a URL
 if __name__ == "__main__":
     from urllib import request
     with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        cod = response.peek()
+        # vy = response.url()
         restype = type(response.read())
-        print(f"""Body response:
-        -type: {restype}
-        """)
+        print(f"""Body response:""")
+        print(f"    -type: {restype}")
+        print(f"    -content: {cod}")
+        if response.headers.get_content_charset() == 'utf-8':
+            print(f"    -utf-8 content: OK")
