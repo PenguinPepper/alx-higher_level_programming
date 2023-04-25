f8edb6a3b2134922775cfa07a709504d977f6330
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-04-25 19:14:06 +0000

    Edit file according to semistandard

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
-search = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+const search = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request(search, function (err, response, body) {
   if (err) throw err;
   const text = JSON.parse(body);
-  console.log(text['title']);
+  console.log(text.title);
 });
