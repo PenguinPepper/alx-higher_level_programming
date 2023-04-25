5febde9d32d62dec9ffc71d16647f3446ab00c47
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-04-25 03:58:20 -0700

    Addjust my script so that it worksdirectly with the body of the response

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,11 +6,10 @@ Script that fetches a URL
 if __name__ == "__main__":
     from urllib import request
     with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        cod = response.peek()
+        cod = response.read()
         # vy = response.url()
-        restype = type(response.read())
+        restype = type(cod)
         print(f"""Body response:""")
         print(f"    - type: {restype}")
         print(f"    - content: {cod}")
-        if response.headers.get_content_charset() == 'utf-8':
-            print(f"    - utf-8 content: OK")
+        print(f"    - utf-8 content: {cod.decode('utf-8')}")
