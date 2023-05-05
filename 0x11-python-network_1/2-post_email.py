ec38fd4b9cf5fdaa12cba199d4ecac65285b13fb
Elizabeth Pfachi <tetlolabtest@gmail.com>
2023-05-05 01:05:54 -0700

    Fix the syntax of the file to meet pycodestyle standards

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,11 +10,10 @@ if __name__ == "__main__":
     import sys
 
     url = sys.argv[1]
-    values = {"email" : sys.argv[2]}
+    values = {"email": sys.argv[2]}
     data = parse.urlencode(values)
     data = data.encode('utf-8')
     req = request.Request(url, data)
     with request.urlopen(req) as response:
         res = response.read()
         print(res.decode('utf-8'))
-
