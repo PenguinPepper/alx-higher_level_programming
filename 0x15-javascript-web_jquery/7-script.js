6e02663963cef37925b7519b9770ce72748c6f47
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-06-05 14:39:17 -0700

    script that fetches the character name from a URL

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,1 +1,7 @@
-$()
+$(function ()
+{
+	url = "https://swapi-api.alx-tools.com/api/people/5/?format=json";
+	$.get(url, funtion(data, textstatus)
+	{
+		$("DIV#character").html(`<div id="character">${data.name}</div>`);
+});
