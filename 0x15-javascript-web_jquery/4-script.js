808c895143e158c67778052276170bed665bbeba
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-06-05 14:00:06 -0700

    Add script that toggles the class of the <header> element when the user clicks on the tag DIV#toggle_header

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,6 +1,18 @@
-$(function()
-{
-	$("DIV#red_header").bind("click", funcion() {
-		$("header").css("color", "#FF0000");
-	});
+$(function () {
+    $("DIV#toggle_header").on("click", function () {
+
+        if ($("header").hasClass("red") && $("header").hasClass("green"))
+            $("header").removeClass("red");
+        else if (!($("header").hasClass("red") && $("header").hasClass("green")))
+            $("header").addClass("green");
+
+        if ($("header").hasClass("red")) {
+            $("header").removeClass("red");
+            $("header").addClass("green");
+        }
+        else if ($("header").hasClass("green")) {
+            $("header").removeClass("green");
+            $("header").addClass("red");
+        }
+    })
 });
