86407dee391aa310bf923312c3ac35d264f06485
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-06-05 15:11:25 -0700

    Add script that fetches and lists the title for all movies by using a URL

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,7 +1,8 @@
-$(function ()
-{
-	url = "https://swapi-api.alx-tools.com/api/films/?format=json";
-	$.get(url, funtion(data, textstatus)
-	{
-		$("DIV#character").html(`<div id="character">${data.name}</div>`);
+$(function () {
+    url = "https://swapi-api.alx-tools.com/api/films/?format=json";
+    $.get(url, function (data, textStatus) {
+        for (let i = 0; i < data.results.length; i++) {
+            $("UL#list_movies").append(`<li>${data.results[i].title}</li>`);
+        }
+    });
 });
