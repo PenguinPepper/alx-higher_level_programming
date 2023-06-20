16fffd5bb2eb83ed050ac96a5d15840d8e0a009d
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-06-20 05:37:13 -0700

    Add test cases for NaN and fix syntax of 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,7 +7,7 @@ def add_integer(a, b=98):
 
     Args:
         a (int): can be an integer or a float
-        b (int): can be an integer or float 
+        b (int): can be an integer or float
 
     Returns:
         The addition of a and b as an int type
@@ -16,11 +16,10 @@ def add_integer(a, b=98):
         raise TypeError('a must be an integer')
     if type(b) not in [int, float]:
         raise TypeError('b must be an integer')
+    """
     if math.isnan(a):
-        raise TypeError('b must be an integer')
+        raise TypeError('a must be an integer')
     if math.isnan(b):
         raise ValueError('b must be an integer')
-    else:
-        a = int(a)
-        b = int(b)
-    return a + b
+    """
+    return int(a) + int(b)
