5ebe63c98953e9614b7c896c61f5f72bfa8b07b9
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-06-21 10:17:01 -0700

    Add file with class that has a method that raises an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module contains class BaseGeometry"""
+
+
+class BaseGeometry():
+    """"Class that inherits from BaseGeometry class
+
+    """
+    def area(self):
+        """"Raises an exception"""
+        raise Exception("area() is not implemented")
