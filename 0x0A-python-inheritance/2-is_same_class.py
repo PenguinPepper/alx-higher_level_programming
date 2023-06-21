75dcbb82f060dff6a835f5b329f2fccba4afe6ac
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-06-21 09:27:31 -0700

    Add function that checks whether a given object is an instance of a given class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr.bin/python3
+"""Module contains is_same_class"""
+
+
+def is_same_class(obj, a_class):
+    """Function tat checks whether an onbject is
+    exactly an instance of a class or not
+    
+    Args:
+        obj (object): object to be checked
+        a_class (class): class pbject to be comapred to obj
+
+    Returns:
+        bool: True if obj is an instance of given class
+    """
+    if type(obj) is a_class:
+        return True
