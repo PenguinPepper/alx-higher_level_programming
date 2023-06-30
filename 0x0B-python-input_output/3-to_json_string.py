229377539b392c259327e6eed746fb442cd5c126
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-06-30 04:22:47 -0700

    Add 3-to_json_string.py that jsonifies an object and returns it

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module contains function that jsonifies a given object"""
+
+
+import json
+def to_json_string(my_obj):
+    """"Function jsoinifies an object
+
+    Args:
+        my_obj(obj): object to be jsonified
+    """
+    return json.dumps(my_obj)
