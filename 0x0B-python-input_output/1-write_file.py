c2ec542e8cc9d67f7b68e7585facb3f310df4cfd
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-06-30 03:30:41 -0700

    Add file that writes to a new file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/us/bin/python3
+"""Module contains a function that writes a file"""
+
+
+def write_file(filename="", text=""):
+    """"Function writes to a file
+
+    Args:
+        filename(str): name of file to be opened
+        text(str): text to be written to the file
+    """
+    with open(filename, mode='w', encoding='utf-8') as f:
+        f.write(text)
+    return(len(text))
