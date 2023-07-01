6ead14593a29e2f7beff548c118eae0133634e35
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-07-01 01:35:14 -0700

    Add file that writes an object to a file in json format

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin.python3
+"""Module contains a function that writes JSON objects to a file"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Funtion writes an object to a text file in JSON form
+
+    Args:
+        my_obj(obj): object to be written to file
+        filename(file): file to write to
+    """
+    with open(filename, mode='w', encoding='utf-8') as f:
+        json.dump(my_obj, f)
