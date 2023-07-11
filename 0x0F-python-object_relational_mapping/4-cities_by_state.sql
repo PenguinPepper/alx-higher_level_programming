7dd125b5c2b19b9a25ddf11b9cbc1cc595c94fed
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-07-11 06:37:45 -0700

    Ädd script that lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.sql
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.sql
@@ -1,6 +1,6 @@
--- Create states table in hbtn_0e_0_usa with some data
-CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
-USE hbtn_0e_0_usa;
+-- Create states table in hbtn_0e_4_usa with some data
+CREATE DATABASE IF NOT EXISTS hbtn_0e_4_usa;
+USE hbtn_0e_4_usa;
 CREATE TABLE IF NOT EXISTS states ( 
     id INT NOT NULL AUTO_INCREMENT, 
     name VARCHAR(256) NOT NULL,
@@ -8,8 +8,8 @@ CREATE TABLE IF NOT EXISTS states (
 );
 INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
 
-CREATE TABLE IF NOT EXISTS cities (
-    id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS cities ( 
+    id INT NOT NULL AUTO_INCREMENT, 
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id),
