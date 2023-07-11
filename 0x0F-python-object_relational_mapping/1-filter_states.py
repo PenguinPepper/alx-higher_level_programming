b9573536eeb7c55a772264cabaa289ec2ad83a27
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-07-11 00:35:21 -0700

    Üse underscore instead

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     databse = MySQLdb.connect(host="localhost", user=sys.argv[1],
                               passwd=sys.argv[2], db=sys.argv[3])
     cur = databse.cursor()
-    sqcmd = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
+    sqcmd = "SELECT * FROM states WHERE name LIKE 'N_' ORDER BY states.id ASC"
     cur.execute(sqcmd)
     rows = cur.fetchall()
     for i in rows:
