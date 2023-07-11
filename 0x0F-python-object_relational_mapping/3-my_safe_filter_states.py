9796f3940f33615e4c88199144b1004bb5ab0cfb
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-07-11 05:52:04 -0700

    Parameterize query to prevent sql injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
 """
-write a script that takes in arguments and displays all values in the states table
-of hbtn_0e_0_usa where name matches the argument and is MYSQL injection safe
+script that takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument
 """
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    databse = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                              passwd=sys.argv[2], db=sys.argv[3])
+    cur = databse.cursor()
+    word = "{}".format(sys.argv[4])
+    cmd = "SELECT * FROM states WHERE name \
+            LIKE BINARY %s ORDER BY states.id ASC"
+    cur.execute(cmd, (word,))
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    cur.close()
+    databse.close()
